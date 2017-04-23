d2aecf2c6f9de2af31a21c2c2e311310d84082ae
duwupeng <865361183@qq.com>
2017-04-23 18:59:14 +0800

    测试

--- src/main/java/com/lg/controllers/WxPayController.java
+++ src/main/java/com/lg/controllers/WxPayController.java
@@ -6,9 +6,11 @@ import com.lg.domain.WeixinLoginUser;
 import com.lg.services.Oauth;
 import com.lg.wechat.model.message.req.UserInfo;
 import com.lg.wechat.model.message.req.WxPaySendData;
+import com.lg.wechat.util.WeChatUtil;
 import com.lg.wechat.util.WxSign;
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.xml.DomDriver;
+import org.apache.commons.io.IOUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -103,8 +105,6 @@ public class WxPayController {
         // 由code获取access_token等信息
         String str = oauth.getToken(WechatConst.appID, WechatConst.appSecret,code);
 
-        System.out.println("由code获取access_token等信息" + str );
-
         // 解析返回的json数据,获取所需的信息
         String openID = (String) JSON.parseObject(str, Map.class).get("openid");
         String accessToken = (String) JSON.parseObject(str, Map.class).get("access_token");
@@ -127,151 +127,149 @@ public class WxPayController {
         // 返回weixinLoginUser对象
         return weixinLoginUser;
     }
-//
-//
-//    /**
-//     　　* 点击确认充值 统一下单,获得预付id(prepay_id)
-//     　　* @param request
-//     　　* @param response
-//     　　* @return
-//     　　 */
-//   @ResponseBody
-//   @RequestMapping({"pay"})
-//    public WxPaySendData prePay(HttpServletRequest request, HttpServletResponse response, String openId){
-//        WxPaySendData result = new WxPaySendData();
-//        openId = des.getDesString(openId);
-//        try {
-//    //商户订单号
-//    String out_trade_no = WeChatUtil.getOut_trade_no();
-//   //产品价格,单位：分
-//   Integer total_fee = 1;
-//   //客户端ip
-//    String ip = HttpUtil.getIpAddr(request);
-//    //支付成功后回调的url地址
-//    String notify_url = "http://你的域名/odao-weixin-site/wxOfficialAccountsPay/callback.do";
-//
-//    //统一下单
-//    String strResult = WeChatUtil.unifiedorder("testPay", out_trade_no, total_fee, ip, notify_url,openId);
-//
-//    //解析xml
-//     XStream stream = new XStream(new DomDriver());
-//    stream.alias("xml", WxPaySendData.class);
-//    WxPaySendData wxReturnData = (WxPaySendData)stream.fromXML(strResult);
-//
-//    //两者都为SUCCESS才能获取prepay_id
-//    if( wxReturnData.getResult_code().equals("SUCCESS") && wxReturnData.getReturn_code().equals("SUCCESS") ){
-//                //业务逻辑，写入订单日志(你自己的业务) .....
-//
-//                String timeStamp = WeChatUtil.getTimeStamp();//时间戳
-//                String nonce_str = WeChatUtil.getNonceStr();//随机字符串
-//    //注：上面这两个参数，一定要拿出来作为后续的value，不能每步都创建新的时间戳跟随机字符串，不然H5调支付API，会报签名参数错误
-//                result.setResult_code(wxReturnData.getResult_code());
-//                result.setAppid(GlobalThreadLocal.getSiteConfig().getWeixin_appId());
-//                result.setTimeStamp(timeStamp);
-//                result.setNonce_str(nonce_str);
-//                result.setPackageStr("prepay_id="+wxReturnData.getPrepay_id());
-//                result.setSignType("MD5");
-//
-////                WeChatUtil.unifiedorder(1) 下单操作中，也有签名操作，那个只针对统一下单，要区别于下面的paySign
-//                //第二次签名,将微信返回的数据再进行签名
-//                SortedMap<Object,Object> signMap = new TreeMap<Object,Object>();
-//                signMap.put("appId", GlobalThreadLocal.getSiteConfig().getWeixin_appId());
-//                signMap.put("timeStamp", timeStamp);
-//                signMap.put("nonceStr", nonce_str);
-//                signMap.put("package", "prepay_id="+wxReturnData.getPrepay_id());  //注：看清楚，值为：prepay_id=xxx,别直接放成了wxReturnData.getPrepay_id()
-//                signMap.put("signType", "MD5");
-//                String paySign = WxSign.createSign(signMap,  WeChatConst.KEY);//支付签名
-//
-//                result.setSign(paySign);
-//            }else{
-//                result.setResult_code("fail");
-//            }
-//      } catch (Exception e) {
-//     e.printStackTrace();
-//    }
-//     return result;
-//    }
-//
-//    /**
-//     * 支付回调接口
-//     * @param request
-//     * @return
-//     */
-//    @RequestMapping("/callback")
-//    public void callBack(HttpServletRequest request, HttpServletResponse response){
-//   response.setContentType("text/xml;charset=UTF-8");
-//    try {
-//      InputStream is = request.getInputStream();
-//      String result = IOUtils.toString(is, "UTF-8");
-//            if("".equals(result)){
-//                response.getWriter().write("<xm><return_code><![CDATA[FAIL]]></return_code><return_msg><![CDATA[参数错误！]]></return_msg></xml>");
-//                return ;
-//            }
-//     //解析xml
-//    XStream stream = new XStream(new DomDriver());
-//    stream.alias("xml", WxPaySendData.class);
-//    WxPaySendData wxPaySendData = (WxPaySendData)stream.fromXML(result);
-//    System.out.println(wxPaySendData.toString());
-//
-//        String appid = wxPaySendData.getAppid();
-//        String mch_id =wxPaySendData.getMch_id();
-//        String nonce_str = wxPaySendData.getNonce_str();
-//            String out_trade_no = wxPaySendData.getOut_trade_no();
-//            String total_fee = wxPaySendData.getTotal_fee();
-//            //double money = DBUtil.getDBDouble(DBUtil.getDBInt(wxPaySendData.getTotal_fee())/100.0);
-//            String trade_type = wxPaySendData.getTrade_type();
-//            String openid =wxPaySendData.getOpenid();
-//            String return_code = wxPaySendData.getReturn_code();
-//            String result_code = wxPaySendData.getResult_code();
-//            String bank_type = wxPaySendData.getBank_type();
-//            Integer cash_fee = wxPaySendData.getCash_fee();
-//            String fee_type = wxPaySendData.getFee_type();
-//            String is_subscribe = wxPaySendData.getIs_subscribe();
-//            String time_end = wxPaySendData.getTime_end();
-//            String transaction_id = wxPaySendData.getTransaction_id();
-//            String sign = wxPaySendData.getSign();
-//
-//            //签名验证
-//            SortedMap<Object,Object> parameters = new TreeMap<Object,Object>();
-//            parameters.put("appid",appid);
-//            parameters.put("mch_id",mch_id);
-//            parameters.put("nonce_str",nonce_str);
-//            parameters.put("out_trade_no",out_trade_no);
-//            parameters.put("total_fee",total_fee);
-//            parameters.put("trade_type",trade_type);
-//            parameters.put("openid",openid);
-//            parameters.put("return_code",return_code);
-//            parameters.put("result_code",result_code);
-//            parameters.put("bank_type",bank_type);
-//            parameters.put("cash_fee",cash_fee);
-//            parameters.put("fee_type",fee_type);
-//            parameters.put("is_subscribe",is_subscribe);
-//            parameters.put("time_end",time_end);
-//            parameters.put("transaction_id",transaction_id);
-//
-//            String sign2 = WxSign.createSign(parameters, WechatConst.KEY);
-//
-//            if(sign.equals(sign2)){//校验签名，两者需要一致，防止别人绕过支付操作，不付钱直接调用你的业务，不然，哈哈，你老板会很开心的 233333.。。。
-//                if(return_code.equals("SUCCESS") && result_code.equals("SUCCESS")){
-//                    //业务逻辑(先判断数据库中订单号是否存在，并且订单状态为未支付状态)
-//           //do something ...
-//
-//
-//                    //request.setAttribute("out_trade_no", out_trade_no);
-//                    //通知微信.异步确认成功.必写.不然会一直通知后台.八次之后就认为交易失败了.
-//                    response.getWriter().write("<xml><return_code><![CDATA[SUCCESS]]></return_code><return_msg><![CDATA[OK]]></return_msg></xml>");
-//                }else{
-//                    response.getWriter().write("<xml><return_code><![CDATA[FAIL]]></return_code><return_msg><![CDATA[交易失败]]></return_msg></xml>");
-//                }
-//            }else{
-//                //通知微信.异步确认成功.必写.不然会一直通知后台.八次之后就认为交易失败了.
-//                response.getWriter().write("<xml><return_code><![CDATA[FAIL]]></return_code><return_msg><![CDATA[签名校验失败]]></return_msg></xml>");
-//            }
-//            response.getWriter().flush();
-//            response.getWriter().close();
-//            return ;
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
+
+
+    /**
+     　　* 点击确认充值 统一下单,获得预付id(prepay_id)
+     　　* @param request
+     　　* @param response
+     　　* @return
+     　　 */
+   @ResponseBody
+   @RequestMapping({"pay"})
+    public WxPaySendData prePay(HttpServletRequest request, HttpServletResponse response, String openId){
+        WxPaySendData result = new WxPaySendData();
+        try {
+            //商户订单号
+            String out_trade_no = WeChatUtil.getOut_trade_no();
+           //产品价格,单位：分
+           Integer total_fee = 1;
+           //客户端ip
+            String ip = request.getRemoteAddr();
+            //支付成功后回调的url地址
+            String notify_url = "http://easygoleagal.com.cn/callback";
+
+            //统一下单
+            String strResult = WeChatUtil.unifiedorder("testPay", out_trade_no, total_fee, ip, notify_url,openId);
+
+            //解析xml
+             XStream stream = new XStream(new DomDriver());
+            stream.alias("xml", WxPaySendData.class);
+            WxPaySendData wxReturnData = (WxPaySendData)stream.fromXML(strResult);
+
+            //两者都为SUCCESS才能获取prepay_id
+            if( wxReturnData.getResult_code().equals("SUCCESS") && wxReturnData.getReturn_code().equals("SUCCESS") ){
+                        //业务逻辑，写入订单日志(你自己的业务) .....
+                String timeStamp = WeChatUtil.getTimeStamp();//时间戳
+                String nonce_str = WeChatUtil.getNonceStr();//随机字符串
+        //注：上面这两个参数，一定要拿出来作为后续的value，不能每步都创建新的时间戳跟随机字符串，不然H5调支付API，会报签名参数错误
+                result.setResult_code(wxReturnData.getResult_code());
+                result.setAppid(WechatConst.appID);
+                result.setTimeStamp(timeStamp);
+                result.setNonce_str(nonce_str);
+                result.setPackageStr("prepay_id="+wxReturnData.getPrepay_id());
+                result.setSignType("MD5");
+
+        //                WeChatUtil.unifiedorder(1) 下单操作中，也有签名操作，那个只针对统一下单，要区别于下面的paySign
+                //第二次签名,将微信返回的数据再进行签名
+                SortedMap<Object,Object> signMap = new TreeMap<Object,Object>();
+                signMap.put("appId", WechatConst.appID);
+                signMap.put("timeStamp", timeStamp);
+                signMap.put("nonceStr", nonce_str);
+                signMap.put("package", "prepay_id="+wxReturnData.getPrepay_id());  //注：看清楚，值为：prepay_id=xxx,别直接放成了wxReturnData.getPrepay_id()
+                signMap.put("signType", "MD5");
+                String paySign = WxSign.createSign(signMap,  WechatConst.KEY);//支付签名
+
+                result.setSign(paySign);
+            }else{
+                    result.setResult_code("fail");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+      }
+         return result;
+    }
+
+    /**
+     * 支付回调接口
+     * @param request
+     * @return
+     */
+    @RequestMapping("/callback")
+    public void callBack(HttpServletRequest request, HttpServletResponse response){
+   response.setContentType("text/xml;charset=UTF-8");
+    try {
+      InputStream is = request.getInputStream();
+      String result = IOUtils.toString(is, "UTF-8");
+            if("".equals(result)){
+                response.getWriter().write("<xm><return_code><![CDATA[FAIL]]></return_code><return_msg><![CDATA[参数错误！]]></return_msg></xml>");
+                return ;
+            }
+     //解析xml
+    XStream stream = new XStream(new DomDriver());
+    stream.alias("xml", WxPaySendData.class);
+    WxPaySendData wxPaySendData = (WxPaySendData)stream.fromXML(result);
+    System.out.println(wxPaySendData.toString());
+
+        String appid = wxPaySendData.getAppid();
+        String mch_id =wxPaySendData.getMch_id();
+        String nonce_str = wxPaySendData.getNonce_str();
+            String out_trade_no = wxPaySendData.getOut_trade_no();
+            String total_fee = wxPaySendData.getTotal_fee();
+            //double money = DBUtil.getDBDouble(DBUtil.getDBInt(wxPaySendData.getTotal_fee())/100.0);
+            String trade_type = wxPaySendData.getTrade_type();
+            String openid =wxPaySendData.getOpenid();
+            String return_code = wxPaySendData.getReturn_code();
+            String result_code = wxPaySendData.getResult_code();
+            String bank_type = wxPaySendData.getBank_type();
+            Integer cash_fee = wxPaySendData.getCash_fee();
+            String fee_type = wxPaySendData.getFee_type();
+            String is_subscribe = wxPaySendData.getIs_subscribe();
+            String time_end = wxPaySendData.getTime_end();
+            String transaction_id = wxPaySendData.getTransaction_id();
+            String sign = wxPaySendData.getSign();
+
+            //签名验证
+            SortedMap<Object,Object> parameters = new TreeMap<Object,Object>();
+            parameters.put("appid",appid);
+            parameters.put("mch_id",mch_id);
+            parameters.put("nonce_str",nonce_str);
+            parameters.put("out_trade_no",out_trade_no);
+            parameters.put("total_fee",total_fee);
+            parameters.put("trade_type",trade_type);
+            parameters.put("openid",openid);
+            parameters.put("return_code",return_code);
+            parameters.put("result_code",result_code);
+            parameters.put("bank_type",bank_type);
+            parameters.put("cash_fee",cash_fee);
+            parameters.put("fee_type",fee_type);
+            parameters.put("is_subscribe",is_subscribe);
+            parameters.put("time_end",time_end);
+            parameters.put("transaction_id",transaction_id);
+
+            String sign2 = WxSign.createSign(parameters, WechatConst.KEY);
+
+            if(sign.equals(sign2)){//校验签名，两者需要一致，防止别人绕过支付操作，不付钱直接调用你的业务，不然，哈哈，你老板会很开心的 233333.。。。
+                if(return_code.equals("SUCCESS") && result_code.equals("SUCCESS")){
+                    //业务逻辑(先判断数据库中订单号是否存在，并且订单状态为未支付状态)
+           //do something ...
+
+
+                    //request.setAttribute("out_trade_no", out_trade_no);
+                    //通知微信.异步确认成功.必写.不然会一直通知后台.八次之后就认为交易失败了.
+                    response.getWriter().write("<xml><return_code><![CDATA[SUCCESS]]></return_code><return_msg><![CDATA[OK]]></return_msg></xml>");
+                }else{
+                    response.getWriter().write("<xml><return_code><![CDATA[FAIL]]></return_code><return_msg><![CDATA[交易失败]]></return_msg></xml>");
+                }
+            }else{
+                //通知微信.异步确认成功.必写.不然会一直通知后台.八次之后就认为交易失败了.
+                response.getWriter().write("<xml><return_code><![CDATA[FAIL]]></return_code><return_msg><![CDATA[签名校验失败]]></return_msg></xml>");
+            }
+            response.getWriter().flush();
+            response.getWriter().close();
+            return ;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/com/lg/wechat/util/WeChatUtil.java
+++ src/main/java/com/lg/wechat/util/WeChatUtil.java
@@ -1,164 +1,188 @@
-//package com.lg.wechat.util;
-//
-//import com.lg.consts.WechatConst;
-//import org.slf4j.Logger;
-//
-//import java.io.IOException;
-//import java.text.DateFormat;
-//import java.text.SimpleDateFormat;
-//import java.util.Date;
-//import java.util.SortedMap;
-//import java.util.TreeMap;
-//
-///**
-// * Created by mac on 17/3/17.
-// */
-//public class WeChatUtil {
-//
-//    private static final Logger logger = Logger.getLogger(WeChatUtil.class);
-//
-//    /**
-//     * 统一下单
-//     * 获得PrePayId
-//     * @param body   商品或支付单简要描述
-//     * @param out_trade_no 商户系统内部的订单号,32个字符内、可包含字母
-//     * @param total_fee  订单总金额，单位为分
-//     * @param IP    APP和网页支付提交用户端ip
-//     * @param notify_url 接收微信支付异步通知回调地址
-//     * @param openid 用户openId
-//     * @throws IOException
-//     */
-//    public static String unifiedorder(String body,String out_trade_no,Integer total_fee,String ip,String notify_url,String openId)throws IOException {
-//        /**
-//         * 设置访问路径
-//         */
-//        HttpPost httppost = new HttpPost("https://api.mch.weixin.qq.com/pay/unifiedorder");
-//        String nonce_str = getNonceStr();//随机数据
-//        SortedMap<Object,Object> parameters = new TreeMap<Object,Object>();
-//        /**
-//         * 组装请求参数
-//         * 按照ASCII排序
-//         */
-//        parameters.put("appid",GlobalThreadLocal.getSiteConfig().getWeixin_appId() );
-//        parameters.put("body", body);
-//        parameters.put("mch_id", WechatConst.MCH_ID );
-//        parameters.put("nonce_str", nonce_str);
-//        parameters.put("out_trade_no", out_trade_no);
-//        parameters.put("notify_url", notify_url);
-//        parameters.put("spbill_create_ip", ip);
-//        parameters.put("total_fee",total_fee.toString() );
-//        parameters.put("trade_type",WeChatConst.TRADE_TYPE );
-//        parameters.put("openid", openId);
-//
-//        String sign = WxSign.createSign(parameters, WeChatConst.KEY);
-//
-//        /**
-//         * 组装XML
-//         */
-//        StringBuilder sb = new StringBuilder("");
-//        sb.append("<xml>");
-//        setXmlKV(sb,"appid",GlobalThreadLocal.getSiteConfig().getWeixin_appId());
-//        setXmlKV(sb,"body",body);
-//        setXmlKV(sb,"mch_id",WeChatConst.MCH_ID);
-//        setXmlKV(sb,"nonce_str",nonce_str);
-//        setXmlKV(sb,"notify_url",notify_url);
-//        setXmlKV(sb,"out_trade_no",out_trade_no);
-//        setXmlKV(sb,"spbill_create_ip",ip);
-//        setXmlKV(sb,"total_fee",total_fee.toString());
-//        setXmlKV(sb,"trade_type",WeChatConst.TRADE_TYPE);
-//        setXmlKV(sb,"sign",sign);
-//        setXmlKV(sb,"openid",openId);
-//        sb.append("</xml>");
-//
-//        StringEntity reqEntity = new StringEntity(new String (sb.toString().getBytes("UTF-8"),"ISO8859-1"));//这个处理是为了防止传中文的时候出现签名错误
-//        httppost.setEntity(reqEntity);
-//        DefaultHttpClient httpclient = new DefaultHttpClient();
-//        HttpResponse response = httpclient.execute(httppost);
-//        String strResult = EntityUtils.toString(response.getEntity(), Charset.forName("utf-8"));
-//
-//        return strResult;
-//
-//    }
-//
-//
-//    //获得随机字符串
-//    public static String getNonceStr(){
-//        Random random = new Random();
-//        return MD5Util.MD5Encode(String.valueOf(random.nextInt(10000)), "UTF-8");
-//    }
-//
-//    //插入XML标签
-//    public static StringBuilder setXmlKV(StringBuilder sb,String Key,String value){
-//        sb.append("<");
-//        sb.append(Key);
-//        sb.append(">");
-//
-//        sb.append(value);
-//
-//        sb.append("</");
-//        sb.append(Key);
-//        sb.append(">");
-//
-//        return sb;
-//    }
-//
-//    //解析XML  获得 PrePayId
-//    public static String getPrePayId(String xml){
-//        int start = xml.indexOf("<prepay_id>");
-//        int end = xml.indexOf("</prepay_id>");
-//        if(start < 0 && end < 0){
-//            return null;
-//        }
-//        return xml.substring(start + "<prepay_id>".length(),end).replace("<![CDATA[","").replace("]]>","");
-//    }
-//
-//    //商户订单号
-//    public static String getOut_trade_no(){
-//        DateFormat df = new SimpleDateFormat("yyyyMMddHHmmssSSS");
-//        return df.format(new Date()) + RandomChars.getRandomNumber(7);
-//    }
-//
-//    //时间戳
-//    public static String getTimeStamp() {
-//        return String.valueOf(System.currentTimeMillis() / 1000);
-//    }
-//
-//    //随机4位数字
-//    public static int buildRandom(int length) {
-//        int num = 1;
-//        double random = Math.random();
-//        if (random < 0.1) {
-//            random = random + 0.1;
-//        }
-//        for (int i = 0; i < length; i++) {
-//            num = num * 10;
-//        }
-//        return (int) ((random * num));
-//    }
-//
-//    public static String inputStream2String(InputStream inStream, String encoding){
-//        String result = null;
-//        try {
-//            if(inStream != null){
-//                ByteArrayOutputStream outStream = new ByteArrayOutputStream();
-//                byte[] tempBytes = new byte[1024];
-//                int count = -1;
-//                while((count = inStream.read(tempBytes, 0, 1024)) != -1){
-//                    outStream.write(tempBytes, 0, count);
-//                }
-//                tempBytes = null;
-//                outStream.flush();
-//                result = new String(outStream.toByteArray(), encoding);
-//            }
-//        } catch (Exception e) {
-//            result = null;
-//        }
-//        return result;
-//    }
-//
-//    public static void main(String[] args) {
-//        System.out.println(getOut_trade_no());
-//    }
-//
-//
-//}
+package com.lg.wechat.util;
+
+import com.lg.consts.WechatConst;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+/**
+ * Created by mac on 17/3/17.
+ */
+public class WeChatUtil {
+
+    /**class
+     * 统一下单
+     * 获得PrePayId
+     * @param body   商品或支付单简要描述
+     * @param out_trade_no 商户系统内部的订单号,32个字符内、可包含字母
+     * @param total_fee  订单总金额，单位为分
+     * @param ip    APP和网页支付提交用户端ip
+     * @param notify_url 接收微信支付异步通知回调地址
+     * @param openId 用户openId
+     * @throws IOException
+     */
+    public static String unifiedorder(String body,String out_trade_no,Integer total_fee,String ip,
+                                      String notify_url,String openId)throws IOException {
+        /**
+         * 设置访问路径
+         */
+        HttpPost httppost = new HttpPost("https://api.mch.weixin.qq.com/pay/unifiedorder");
+        String nonce_str = getNonceStr();//随机数据
+        SortedMap<Object,Object> parameters = new TreeMap<Object,Object>();
+        /**
+         * 组装请求参数
+         * 按照ASCII排序
+         */
+        parameters.put("appid",WechatConst.appID);
+        parameters.put("body", body);
+        parameters.put("mch_id", WechatConst.MCH_ID );
+        parameters.put("nonce_str", nonce_str);
+        parameters.put("out_trade_no", out_trade_no);
+        parameters.put("notify_url", notify_url);
+        parameters.put("spbill_create_ip", ip);
+        parameters.put("total_fee",total_fee.toString() );
+        parameters.put("trade_type",WechatConst.TRADE_TYPE );
+        parameters.put("openid", openId);
+
+        String sign = WxSign.createSign(parameters, WechatConst.KEY);
+
+        /**
+         * 组装XML
+         */
+        StringBuilder sb = new StringBuilder("");
+        sb.append("<xml>");
+        setXmlKV(sb,"appid",WechatConst.appID);
+        setXmlKV(sb,"body",body);
+        setXmlKV(sb,"mch_id",WechatConst.MCH_ID);
+        setXmlKV(sb,"nonce_str",nonce_str);
+        setXmlKV(sb,"notify_url",notify_url);
+        setXmlKV(sb,"out_trade_no",out_trade_no);
+        setXmlKV(sb,"spbill_create_ip",ip);
+        setXmlKV(sb,"total_fee",total_fee.toString());
+        setXmlKV(sb,"trade_type",WechatConst.TRADE_TYPE);
+        setXmlKV(sb,"sign",sign);
+        setXmlKV(sb,"openid",openId);
+        sb.append("</xml>");
+
+        StringEntity reqEntity = new StringEntity(new String (sb.toString().getBytes("UTF-8"),"ISO8859-1"));//这个处理是为了防止传中文的时候出现签名错误
+        httppost.setEntity(reqEntity);
+        DefaultHttpClient httpclient = new DefaultHttpClient();
+        HttpResponse response = httpclient.execute(httppost);
+        String strResult = EntityUtils.toString(response.getEntity(), Charset.forName("utf-8"));
+
+        return strResult;
+
+    }
+
+
+    //获得随机字符串
+    public static String getNonceStr(){
+        Random random = new Random();
+        return MD5Util.MD5Encode(String.valueOf(random.nextInt(10000)), "UTF-8");
+    }
+
+    //插入XML标签
+    public static StringBuilder setXmlKV(StringBuilder sb,String Key,String value){
+        sb.append("<");
+        sb.append(Key);
+        sb.append(">");
+
+        sb.append(value);
+
+        sb.append("</");
+        sb.append(Key);
+        sb.append(">");
+
+        return sb;
+    }
+
+    //解析XML  获得 PrePayId
+    public static String getPrePayId(String xml){
+        int start = xml.indexOf("<prepay_id>");
+        int end = xml.indexOf("</prepay_id>");
+        if(start < 0 && end < 0){
+            return null;
+        }
+        return xml.substring(start + "<prepay_id>".length(),end).replace("<![CDATA[","").replace("]]>","");
+    }
+
+    //商户订单号
+    public static String getOut_trade_no(){
+        DateFormat df = new SimpleDateFormat("yyyyMMddHHmmssSSS");
+        return df.format(new Date()) + getRandomString(7);
+    }
+
+
+    private static int getRandom(int count) {
+      return (int) Math.round(Math.random() * (count));
+    }
+
+    private static String string = "abcdefghijklmnopqrstuvwxyz";
+
+    private static String getRandomString(int length){
+        StringBuffer sb = new StringBuffer();
+        int len = string.length();
+        for (int i = 0; i < length; i++) {
+            sb.append(string.charAt(getRandom(len-1)));
+        }
+        return sb.toString();
+    }
+
+    //时间戳
+    public static String getTimeStamp() {
+        return String.valueOf(System.currentTimeMillis() / 1000);
+    }
+
+    //随机4位数字
+    public static int buildRandom(int length) {
+        int num = 1;
+        double random = Math.random();
+        if (random < 0.1) {
+            random = random + 0.1;
+        }
+        for (int i = 0; i < length; i++) {
+            num = num * 10;
+        }
+        return (int) ((random * num));
+    }
+
+    public static String inputStream2String(InputStream inStream, String encoding){
+        String result = null;
+        try {
+            if(inStream != null){
+                ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+                byte[] tempBytes = new byte[1024];
+                int count = -1;
+                while((count = inStream.read(tempBytes, 0, 1024)) != -1){
+                    outStream.write(tempBytes, 0, count);
+                }
+                tempBytes = null;
+                outStream.flush();
+                result = new String(outStream.toByteArray(), encoding);
+            }
+        } catch (Exception e) {
+            result = null;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getOut_trade_no());
+    }
+
+
+}
